bedb0a5ebc672d7e3c6f4112df03bb4f817e6a44
Lunar-0rbit <33024675+Lunar-0rbit@users.noreply.github.com>
2017-11-01 10:00:54 +0100

    step
    ste_up_down

--- step_down.m
+++ step_down.m
@@ -1,11 +1,10 @@
-function [ u1,v1 ] = step_down(u1,v1,M,N)
+function [ v1 ] = step_down(u1,M,N)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
-u1 = reshape(u1,M,N); v1 = reshape(v1,M,N);
-u1 = u1([1:M/2]*2-1,[1:N/2]*2-1);
-v1= v1([1:M/2]*2-1,[1:N/2]*2-1);
-u1=u1(:);
-v1=v1(:);
+u1 = reshape(u1,M,N);
+v1 = 1/4*(u1(1:2:M,1:2:N)+u1(2:2:M,1:2:N)+u1(1:2:M,2:2:N)+u1(2:2:M,2:2:N));
+%u1=u1(:);
+
 end
 
--- step_up.m
+++ step_up.m
@@ -1,18 +1,13 @@
-function [ u1,v1 ] = step_up( u1_p,v1_p,M,N )
+function [ v1 ] = step_up( u1,M,N )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
-u1_p = reshape(u1_p,M/2,N/2);
-v1_p = reshape(v1_p,M/2,N/2);
-u1 = zeros(2*M,2*N);
-v1 = zeros(2*M+2,2*N+2);
-u1([1:M/2]*2,[1:N/2]*2)=u1_p;
-v1([1:M/2]*2,[1:N/2]*2)=v1_p;
-v1=conv2(u1,1/8*[2 4 2;4 8 4;2 4 2]);
-v1=conv2(v1,1/8*[2 4 2;4 8 4;2 4 2]);
-u1=u1(2:M+1,2:N+1);
-v1=v1(2:M+1,2:N+1);
-u1=u1(:);
-v1=v1(:);
+
+u1=reshape(u1,M,N);
+v1=zeros(2*M,2*N);
+v1(1:2:2*M,1:2:2*N)=u1;
+v1(2:2:2*M,1:2:2*N)=u1;
+v1(1:2:2*M,2:2:2*N)=u1;
+v1(2:2:2*M,2:2:2*N)=u1;
 
 end
 
